406de11aeaa4cd8d15c9cce99d09cdefc5993eaa
Nikhil Shukla <nikhilshukla28051991@gmail.com>
2021-04-14 11:28:50 +0530

    Testing from local

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
         //testing from git again
-	public /*test nikhilll*/ static void main(String[] args) {
+	public /*test nikhilll1*/ static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
