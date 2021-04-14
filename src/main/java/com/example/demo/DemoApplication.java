7d1c974e1427b85ea4df641ebae2e313afc9a6ff
nikhilThe-tech <72499314+nikhilThe-tech@users.noreply.github.com>
2021-04-14 11:06:14 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-        //testing from git
+        //testing from git again
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
